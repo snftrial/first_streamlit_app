3a49757855f8de7cab3c9c309d3824d3454ea384
snftrial <131142619+snftrial@users.noreply.github.com>
2023-04-20 15:25:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
-
+import pandas
 streamlit.title('Hello World')
 streamlit.header('Hello')
 streamlit.text('from the outside')
 streamlit.text('...')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
