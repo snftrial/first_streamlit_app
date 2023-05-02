277eb99951a99c606e89bee7a456d31fa50bde60
snftrial <131142619+snftrial@users.noreply.github.com>
2023-05-02 14:14:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,21 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list= my_fruit_list.set_index('Fruit');
 
 #enable select
-fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries']);
+
+fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index));
 fruits_to_show=my_fruit_list.loc[fruits_selected];
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
+
 
 #display df
 streamlit.dataframe(fruits_to_show)
 
 #New section to display api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice=streamlit.text_input('What fruit would you like information about?')
 streamlit.write('The user entered',fruit_choice)
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
+
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
@@ -42,3 +47,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit=streamlit.text_input("What fruit would you like to add ?")
 streamlit.write('Thanks for adding',add_my_fruit)
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
