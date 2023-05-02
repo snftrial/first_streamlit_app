397c383288231776537d48c7b63a09cd1d24d2f6
snftrial <131142619+snftrial@users.noreply.github.com>
2023-05-02 14:06:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The list:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.text("What fruit would you like to add ?")
-add_my_fruit=streamlit.text_input('')
+add_my_fruit=streamlit.text_input("What fruit would you like to add ?")
 streamlit.write('Thanks for adding',add_my_fruit)
