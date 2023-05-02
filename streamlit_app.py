af9af8b6e793723afcb7f671e5e8dbdb2348c62c
snftrial <131142619+snftrial@users.noreply.github.com>
2023-05-02 14:32:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,16 +29,23 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice=streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered',fruit_choice)
-my_cur.execute("insert into fruit_load_list values('from streamlit')");
+try:
+  fruit_choice=streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+except URLError as e:
+  streamlit.error()
+
+  my_cur.execute("insert into fruit_load_list values('from streamlit')");
+
 
 # parse json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show df
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 my_cur.execute("SELECT * from fruit_load_list")
